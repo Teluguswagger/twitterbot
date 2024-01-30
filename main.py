dcd3785e2e07209e4bf630d4ec530666ede17d98
Teluguswagger <158076170+Teluguswagger@users.noreply.github.com>
2024-01-30 11:54:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from datetime import date
 from datetime import timedelta
 from json import dumps
 
-api_key = "API_KEY"
-api_secret = "API_SECRET"
-bearer_token = "BEARER_TOKEN"
-access_token = "ACCESS_TOKEN"
-access_token_secret = "ACCESS_TOKEN_SECRET"
+api_key = "Dcy5qVhM6BoWzSqUm4eC3eD9P"
+api_secret = "HI1qANYmgpYtp6NE3gAyreOh3v1rkIMTQPKtiSzZLmBBEDc1ny"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAACKOsAEAAAAALhaVx5HYqctmec6kTyvUI6ZhZTM%3Dp4fj4HdFUyLOqkz3dODmfIeE2kYuVTPJN01CiPSiTemSUdu7yr"
+access_token = "1751968878341885952-FwHZorPORJNugpZ2lJNOQVGxBHBQsO"
+access_token_secret = "92WuEA3u0SLlK9sokDN7WOLHKrnJ5NrA8BShMGK9S3LB2"
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 auth = tweepy .OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
