d75a2cfd81f36e3a93d20f786b9881cb5df193f4
Teluguswagger <158076170+Teluguswagger@users.noreply.github.com>
2024-01-30 17:51:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ client = tweepy.Client(consumer_key=consumer_key,
                     access_token=access_token,
                     access_token_secret=access_token_secret)
 
+media = client.upload_media(filename='/Users/tharunteja/Downloads/Pushpa_ The Rule - Part 2 (2024)_files/MV5BZTQ3M2QxMTAtMmQ0Yi00YmVmLThkZGYtODBiN2NhNjBhYzcwXkEyXkFqcGdeQXVyMTY0MTg5OTM4._V1_.jpg')
 
 days = date(2024,8,16) - date.today() - timedelta(days = 1)
 cd = str(days.days) + " Days left for #Pushpa2TheRule Rampage \n\n@alluarjun"
-response = client.create_tweet(text=cd)
+response = client.create_tweet(text=cd, media_ids=[media.media_key])
