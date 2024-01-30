4989cd4613dd139e4303864134e928ccd88aa92f
Teluguswagger <158076170+Teluguswagger@users.noreply.github.com>
2024-01-30 17:33:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from datetime import date
 from datetime import timedelta
 from json import dumps
 
-consumer_key = os.environ['PUSHPA_API_KEY']
-consumer_secret = os.environ['PUSHPA_API_SECRET']
-access_token = os.environ['PUSHPA_ACCESS_KEY']
-access_token_secret = os.environ['PUSHPA_ACCESS_TOKEN_SECRET']
+consumer_key = os.environ['API_KEY']
+consumer_secret = os.environ['API_SECRET']
+access_token = os.environ['ACCESS_TOKEN']
+access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
 client = tweepy.Client(consumer_key=consumer_key,
                     consumer_secret=consumer_secret,
