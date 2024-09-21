e677bcf270cdaced80f52d5c88092aebb196f9ad
Teluguswagger <tharunteja2u@gmail.com>
2024-09-21 23:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ consumer_secret = os.environ['API_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
 
-client = tweepy.Client(consumer_key=consumer_key,
-                    consumer_secret=consumer_secret,
-                    access_token=access_token,
-                    access_token_secret=access_token_secret)
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
 
 # Event date
 event_date = date(2024, 12, 6)
